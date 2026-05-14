6b738b3e308319bd77afe105cbcaca9ead850ed7
anton <a.m.vanwezenbeek@gmail.com>
2026-05-14 14:22:34 +0200

    changes after lint error

--- src/create-zip.sh
+++ src/create-zip.sh
@@ -15,7 +15,7 @@ ROOT=$(git rev-parse --show-toplevel)
 version=$($ROOT/src/filter-version.sh)
 
 # generate output in current folder
-output="`pwd`/sp-wex.zip-${version}"
+output="$(pwd)/sp-wex.zip-${version}"
 
 # we have to be in the parent dir for toplevel dir
 cd ../..
@@ -24,11 +24,11 @@ zip -r  ${output} syncped -x "*/build/*" "*/.git*" "*/.cache*" "*/tidy*" "*/swig
 
 # if wex is in /workspaces, use that, otherwise look in home dir
 if [ -d "/workspaces/wex" ]; then
-  cd /workspaces
+  cd /workspaces || exit
 else
   # wex not found in /workspace, looking in home dir
   if [ -d "$HOME/wex" ]; then
-    cd "$HOME"
+    cd "$HOME" || exit
   else
     echo "wex not found, exiting"
     exit 1
